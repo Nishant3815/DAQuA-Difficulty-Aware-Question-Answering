29b08c756903c1593f3262c1770f2a7f0ef2259f
Nishant Raj <nishant95.raj@gmail.com>
2022-02-26 03:48:44 +0000

    Adding codebase to build processed db for hotpotqa

---
+++ hotpot_qa/hotpotqa.sh
@@ -0,0 +1,8 @@
+# Download Hotpot Data
+wget http://curtis.ml.cmu.edu/datasets/hotpot/hotpot_dev_distractor_v1.json
+wget http://curtis.ml.cmu.edu/datasets/hotpot/hotpot_dev_fullwiki_v1.json
+wget http://curtis.ml.cmu.edu/datasets/hotpot/hotpot_train_v1.1.json
+wget http://curtis.ml.cmu.edu/datasets/hotpot/hotpot_test_fullwiki_v1.json
+wget https://nlp.stanford.edu/projects/hotpotqa/enwiki-20171001-pages-meta-current-withlinks-abstracts.tar.bz2
+# Untar the wikidump
+# tar -xjvf enwiki-20171001-pages-meta-current-withlinks-abstracts.tar.bz2
