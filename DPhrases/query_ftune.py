a58be1f4e569789eb4e6195b18c1511ff45ac612
akaysh <ksh.shrm1@gmail.com>
2022-04-19 05:33:22 +0000

    Fixed  issues

--- DPhrases/query_ftune.py
+++ DPhrases/query_ftune.py
@@ -66,12 +66,11 @@ def setup_run():
     if torch.cuda.is_available():
         torch.cuda.manual_seed_all(args.seed)
 
-    wandb.init(project="query-fine-tuning",
-        run = run_id,
-        notes="Query fine tuning for MHop retrieval",
-        tags=["trial"],
-        config=args,
-        entity="daqua")
+    wandb.init(project = "query-fine-tuning",
+        notes = 'Query fine tuning for MHop retrieval',
+        tags = ["trial"],
+        config = args,
+        entity = "daqua")
     
 
     return args, save_path
